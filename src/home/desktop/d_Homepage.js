b0dbcdb985113df9b422da39aa53adb597b74dc2
Norman Buruchara <normanburuchara90@gmail.com>
2024-04-23 22:02:45 +0300

    Env setup
    Setting up different view

---
+++ src/home/desktop/d_Homepage.js
@@ -0,0 +1,34 @@
+import React,{Component} from 'react'
+import styled from 'styled-components'
+import MediaQuery from 'react-responsive'
+
+const Styles = styled.div `
+    
+    // - - FULL PAGE - - //
+
+.fullPage {
+    width: 100%;
+    height: 100vh;
+}
+
+`
+
+export default class DesktopHome extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <Styles>
+                <div className='fullPage'>
+
+                </div>
+            </Styles>
+        )
+    }
+}
+
---
+++ src/home/phablet/p_Homepage.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react'
+import styled from 'styled-components'
+import MediaQuery from 'react-responsive'
+
+const Styles = styled.div `
+    
+    // - - FULL PAGE - - //
+
+.fullPage {
+    width: 100%;
+    height: 100vh;
+}
+
+`
+
+export default class PhabletHome extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <Styles>
+                <div className='fullPage'>
+
+                </div>
+            </Styles>
+        )
+    }
+}
