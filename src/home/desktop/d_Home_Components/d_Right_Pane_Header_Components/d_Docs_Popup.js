9b06eacbd7d64b0887e1762176202cbbe25b86c0
Norman Buruchara <normanburuchara90@gmail.com>
2024-04-29 20:27:32 +0300

    Quick docs feature (more UI tweaks)
    Finished the UI for rendering searched docs (matches WorkOS' UI)

--- src/home/desktop/d_Home_Components/d_Right_Pane_Header_Components/d_Docs_Popup.js
+++ src/home/desktop/d_Home_Components/d_Right_Pane_Header_Components/d_Docs_Popup.js
@@ -84,18 +84,26 @@ const Styles = styled.div  `
 
 .popup-docs-search-text {
     float: left;
-    text-align: center;
-    width: 92%;
+    text-align: left;
+    width: 85%;
+}
+
+.popup-docs-search-close-icon {
+    float: left;
+    text-align: left;
+    width: 7%;
 }
 
     // # BUTTON 
     
 .popup-docs-search button {
+    cursor: text !important;
     width: 100% !important;
     padding: 0.1em !important;
+    padding-right: 0.3em !important;
 }
 
-    // # ICON
+    // # SEARCH ICON
 
 .popup-docs-search-icon img {
     margin-top: 25%;
@@ -115,6 +123,26 @@ const Styles = styled.div  `
 
 .popup-docs-search-text input:focus {
     outline: none;
+}
+
+    // # CLOSE ICON & SPAN 
+
+.popup-docs-search-close-icon span {
+    background-color: transparent;
+    border-radius: 50%;
+    padding-top: 15%;
+    padding-left: 15%;
+    padding-right: 12%;
+}
+
+.popup-docs-search-close-icon span:hover  {
+    background-color: #ccc;
+}
+
+.popup-docs-search-close-icon img {
+    margin-top: 35%;
+    width: 70%;
+    margin-right: 5%;
 }
 
     // - POPUP SEE ALL BUTTON - //
@@ -147,7 +175,7 @@ const Styles = styled.div  `
     text-align: left;
     padding-left: 2.5%;
     padding-right: 2.5%;
-    padding-top: 0px;
+    margin-top: 2%;
     padding-bottom: 0px;
     border-radius: 5px;
     cursor: pointer;
@@ -161,14 +189,15 @@ const Styles = styled.div  `
 }
 
 .searchResultOption {
-    padding-top: 2%;
-    margin-bottom: 2%;
-    font-size: 88.5%;
+    padding-top: 1%;
+    margin-bottom: 0px;
+    font-size: 80.5%;
 }
 
 .searchResultCategory {
-    font-size: 76%;
-    padding-bottom: 3%;
+    font-size: 72%;
+    padding-bottom: 1%;
+    cursor: pointer;
 }
 
     // - CODE SNIPPET SEARCH RESULT - //
@@ -177,8 +206,8 @@ const Styles = styled.div  `
     text-align: left;
     padding-left: 2.5%;
     padding-right: 2.5%;
-    padding-top: 0px;
-    padding-bottom: 0px;
+    padding-top: 0.5%;
+    padding-bottom: 2%;
     border-radius: 5px;
     cursor: pointer;
 }
@@ -218,6 +247,13 @@ const Styles = styled.div  `
     // margin-left: ;
     font-family: poppins;
     // margin-bottom: 7%;
+    font-size: 72.5%;
+}
+
+.codeSnippetLineNoCommand p {
+    text-align: left;
+    margin-top: 0px;
+    font-family: poppins;
     font-size: 72.5%;
 }
 
@@ -274,14 +310,17 @@ export default class Popup extends Component {
         super()
         this.state = {
             searchedData: "",
+            searchCloseBtn: false,
             filteredOptions: [], // Use imported options
             isSearchLoading: false,
             resultsFound: false,
             hoveredResultId: null,
             commandBgColor: "transparent",
-            commandTxtColor: "transparent"
+            commandTxtColor: "transparent",
+            searchBarInFocus: false
         }
         this.trie = new Trie(); // Initialize the trie
+        this.inputRef = React.createRef();
     }
 
     componentDidMount() {
@@ -289,6 +328,32 @@ export default class Popup extends Component {
         for (let option of ResultsData) {
             this.trie.insert(option.name.toLowerCase());
         }
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClickOutside);
+        document.addEventListener('click', this.handleClickOutside);
+    }
+
+    handleClickOutside = (e) => {
+        if (this.inputRef.current && !this.inputRef.current.contains(e.target)) {
+            this.setState({searchBarInFocus: false});
+        }
+    };
+
+    clearSearchBar = () => {
+        this.setState({
+            searchedData: "",
+            searchCloseBtn: false
+        })
+        this.inputRef.current.focus(); // Keep the input in focus
+    }
+
+    searchBarClicked = () => {
+        this.setState({
+            searchBarInFocus: true
+        })
+        this.inputRef.current.focus();
     }
 
     groupBy = (array, key) => {
@@ -304,56 +369,57 @@ export default class Popup extends Component {
     // Clear previous timeout
     clearTimeout(this.searchTimeout);
 
-    this.setState({searchedData: e.target.value, isSearchLoading: true})
+    this.setState({searchedData: e.target.value, isSearchLoading: true, searchCloseBtn: true})
     
-    // Set a new timeout to execute after 500ms
-    this.searchTimeout = setTimeout(() => {
-        if (searchInput.trim() === "") {
-            // Reset filteredOptions and loading state
-            this.setState({
-                searchedData: "",
-                filteredOptions: [],
-                isSearchLoading: false,
-                resultsFound: false
-            });
-        } else {
-            // Show loading screen and start search
-            this.setState({ isSearchLoading: true, searchedData: searchInput }, () => {
-                // Perform search logic
-                const filteredOptions = ResultsData.filter(option => {
-                    const words = option.name.toLowerCase().split(" ");
-                    return words.some(word => word.startsWith(searchInput));
+        // Set a new timeout to execute after 500ms
+        this.searchTimeout = setTimeout(() => {
+            if (searchInput.trim() === "") {
+                // Reset filteredOptions and loading state
+                this.setState({
+                    searchedData: "",
+                    searchCloseBtn: false,
+                    filteredOptions: [],
+                    isSearchLoading: false,
+                    resultsFound: false
                 });
+            } else {
+                // Show loading screen and start search
+                this.setState({ isSearchLoading: true, searchedData: searchInput, searchCloseBtn: true}, () => {
+                    // Perform search logic
+                    const filteredOptions = ResultsData.filter(option => {
+                        const words = option.name.toLowerCase().split(/[\s/:]+/);
+                        return words.some(word => word.startsWith(searchInput));
+                    });
 
-                const resultsFound = filteredOptions.length > 0; // Check if any results were found
+                    const resultsFound = filteredOptions.length > 0; // Check if any results were found
 
-                const highlightedOptions = filteredOptions.map(option => ({
-                    ...option,
-                    highlightedName: this.highlightMatchedCharacters(option, searchInput)
-                }));
+                    const highlightedOptions = filteredOptions.map(option => ({
+                        ...option,
+                        highlightedName: this.highlightMatchedCharacters(option, searchInput)
+                    }));
 
-                const groupedResults = this.groupBy(highlightedOptions, 'category');
+                    const groupedResults = this.groupBy(highlightedOptions, 'category');
 
-                // Construct trie for each category
-                const trieByCategory = {};
-                Object.entries(groupedResults).forEach(([category, options]) => {
-                    trieByCategory[category] = new Trie();
-                    options.forEach(option => {
-                        trieByCategory[category].insert(option.name.toLowerCase());
+                    // Construct trie for each category
+                    const trieByCategory = {};
+                    Object.entries(groupedResults).forEach(([category, options]) => {
+                        trieByCategory[category] = new Trie();
+                        options.forEach(option => {
+                            trieByCategory[category].insert(option.name.toLowerCase());
+                        });
+                    });
+
+                    // Update state after search logic is complete
+                    this.setState({
+                        trieByCategory,
+                        groupedOptions: groupedResults,
+                        filteredOptions: highlightedOptions,
+                        isSearchLoading: false, // Hide loading screen
+                        resultsFound: resultsFound
                     });
                 });
-
-                // Update state after search logic is complete
-                this.setState({
-                    trieByCategory,
-                    groupedOptions: groupedResults,
-                    filteredOptions: highlightedOptions,
-                    isSearchLoading: false, // Hide loading screen
-                    resultsFound: resultsFound
-                });
-            });
-        }
-    }, 500); // Set debounce delay to 500ms
+            }
+        }, 500); // Set debounce delay to 500ms
     };
 
     highlightMatchedCharacters(option, searchInput, isSearchLoading) {
@@ -390,7 +456,7 @@ export default class Popup extends Component {
     }
 
     render () {
-        const { isSearchLoading, groupedOptions, resultsFound, hoveredResultId } = this.state;
+        const { isSearchLoading, groupedOptions, resultsFound, hoveredResultId, searchCloseBtn, searchBarInFocus} = this.state;
         const searchInput = this.state.searchedData.trim().toLowerCase();
         return (
             <Styles>
@@ -403,17 +469,32 @@ export default class Popup extends Component {
                     <div className='popup-container'>
                         <div className='popup-docs-header'>
                             <div className='popup-docs-search'>
-                                <button>
+                                <button
+                                onClick={this.searchBarClicked}
+                                style={{border: searchBarInFocus ? "1px solid #cccccc" : "1px solid transparent", cursor: "text"}}
+                                >
                                     <div className='popup-docs-search-icon'>
                                         <img src='/assets/docs_popup_search_icon.png' alt='no img available'/>
                                     </div>
                                     <div className='popup-docs-search-text'>
                                         <input
+                                        ref={this.inputRef}
                                         id='searchedData'
                                         type="text"
+                                        placeholder='Search the docs...'
                                         value={this.state.searchedData}
                                         onChange={this.handleSearchChange}
+                                        onClick={this.searchBarClicked}
                                         />
+                                    </div>
+                                    <div className='popup-docs-search-close-icon'>
+                                        {searchCloseBtn && 
+                                            <span
+                                            onClick={this.clearSearchBar}
+                                            >
+                                                <img src='/assets/docs_popup_exit_search_icon.png' alt='img not available'/>
+                                            </span>
+                                        }
                                     </div>
                                 </button>
                             </div>
@@ -432,7 +513,7 @@ export default class Popup extends Component {
                                 }
                                 {!isSearchLoading && resultsFound && 
                                     Object.entries(groupedOptions).map(([category, options]) => (
-                                        <div style={{borderBottom: "1px solid #ccc", paddingTop: "3.5%", paddingBottom: "3.5%"}} key={category}>
+                                        <div style={{borderBottom: "1px solid #ccc", paddingTop: "0%", paddingBottom: "2.5%"}} key={category}>
                                             {options.map(option => (
                                                 <div>
                                                     {category !== "Code Snippet" ? 
@@ -444,30 +525,51 @@ export default class Popup extends Component {
                                                         className='searchResultCell' 
                                                         key={option.id}>
                                                             <p className='searchResultOption'>{option.highlightedName}</p>
-                                                            <p className='searchResultCategory'>{category}</p>
+                                                            <p className='searchResultCategory'>{category} {option.subCat1 ? <label style={{cursor: "pointer"}}> {'>'} {option.subCat1}</label> : null } {option.subCat2 ? <label style={{cursor: "pointer"}}>{'>'} {option.subCat2}</label> : null }</p> 
                                                         </div>
                                                     ) : (
-                                                        <div 
-                                                        onMouseEnter={() => this.currentSearchedCellEnter(option.id)}
-                                                        onMouseLeave={this.currentSearchedCellLeave}
-                                                        style={{backgroundColor: hoveredResultId === option.id ? "#F0F1FF" : "transparent", color: hoveredResultId === option.id ? "#1C1C8E" : "black"}} 
-                                                        className='codeSnippetResult'>
-                                                            <div className='codeSnippetCommand'>
-                                                                <span style={{
-                                                                    backgroundColor: option.command !== "DELETE" ? option.command === "GET" ? "#e6f4fe" : "#d8eaed" : "#feeaed",
-                                                                    color: option.command !== "DELETE" ? option.command === "GET" ? "#0072dd"  : "#00815c" : "#cf375b"
-                                                                }}>{option.command}</span>
-                                                            </div>
-                                                            <div 
-                                                            className='codeSnippetLine' 
-                                                            key={option.id}>
-                                                                <p 
-                                                                style={{marginLeft: option.command !== "DELETE" ? option.command === "GET" ? "0px" : "3%" : "7%"}}
-                                                                className='searchResultOption'>/{option.highlightedName}</p>
-                                                                {/* <p className='searchResultCategory'>{category}</p> */}
-                                                            </div>
+                                                        <div>
+                                                            {option.command !== "" ? 
+                                                            (
+                                                                <div 
+                                                                onMouseEnter={() => this.currentSearchedCellEnter(option.id)}
+                                                                onMouseLeave={this.currentSearchedCellLeave}
+                                                                style={{backgroundColor: hoveredResultId === option.id ? "#F0F1FF" : "transparent", color: hoveredResultId === option.id ? "#1C1C8E" : "black"}} 
+                                                                className='codeSnippetResult'>
+                                                                    <div className='codeSnippetCommand'>
+                                                                        <span style={{
+                                                                            backgroundColor: option.command !== "DELETE" ? option.command === "GET" ? "#e6f4fe" : "#d8eaed" : "#feeaed",
+                                                                            color: option.command !== "DELETE" ? option.command === "GET" ? "#0072dd"  : "#00815c" : "#cf375b"
+                                                                        }}>{option.command}</span>
+                                                                    </div>
+                                                                    <div 
+                                                                    className='codeSnippetLine' 
+                                                                    key={option.id}>
+                                                                        <p 
+                                                                        style={{marginLeft: option.command !== "DELETE" ? option.command === "GET" ? "0px" : "3%" : "7%"}}
+                                                                        className='searchResultOption'>/{option.highlightedName}</p>
+                                                                        {/* <p className='searchResultCategory'>{category}</p> */}
+                                                                    </div>
+                                                                </div>
+                                                            ) : (
+                                                                <div 
+                                                                onMouseEnter={() => this.currentSearchedCellEnter(option.id)}
+                                                                onMouseLeave={this.currentSearchedCellLeave}
+                                                                style={{backgroundColor: hoveredResultId === option.id ? "#F0F1FF" : "transparent", color: hoveredResultId === option.id ? "#1C1C8E" : "black"}} 
+                                                                className='codeSnippetResult'>
+                                                                    <div 
+                                                                    className='codeSnippetLineNoCommand' 
+                                                                    key={option.id}>
+                                                                        <p 
+                                                                        // style={{marginLeft: option.command !== "DELETE" ? option.command === "GET" ? "0px" : "3%" : "7%"}}
+                                                                        className='searchResultOption'>{option.highlightedName}</p>
+                                                                        {/* <p className='searchResultCategory'>{category}</p> */}
+                                                                    </div>
+                                                                </div>
+                                                            )}
+                                                            
                                                         </div>
-                                                    ) }
+                                                    )}
                                                     
                                                 </div>
                                             ))}
