273b4e3210bf5821db7d3b263fd11fc6f0beba4c
Norman Buruchara <normanburuchara90@gmail.com>
2024-04-29 22:17:10 +0300

    Quick docs feature (updated search results)

--- src/home/desktop/d_Home_Components/d_Right_Pane_Header_Components/d_Docs_Results.js
+++ src/home/desktop/d_Home_Components/d_Right_Pane_Header_Components/d_Docs_Results.js
@@ -6,37 +6,38 @@ const results = [
 
   { id: 1, name: "auth/factors/:id", category: "Code Snippet", command: "DELETE"},
   { id: 2, name: "auth/factors/:id", category: "Code Snippet", command: "GET"},
-  { id: 3, name: "auth/factors/:id/challenge", category: "Code Snippet", command: "POST"},
-  { id: 4, name: "auth/factors/enroll", category: "Code Snippet", command: "POST"},
-  { id: 5, name: "authentication_challenge", category: "Code Snippet", command: ""},
-  { id: 6, name: "authentication_factor", category: "Code Snippet", command: ""},
-  { id: 7, name: "audit_logs/exports/:id", category: "Code Snippet", command: "GET"},
-  { id: 8, name: "audit_logs/exports/", category: "Code Snippet", command: "POST"},
-  { id: 9, name: "audit_logs_export", category: "Code Snippet", command: ""},
+  { id: 3, name: "auth/factors/:id/verify", category: "Code Snippet", command: "POST"},
+  { id: 4, name: "auth/factors/:id/challenge", category: "Code Snippet", command: "POST"},
+  { id: 5, name: "auth/factors/enroll", category: "Code Snippet", command: "POST"},
+  { id: 6, name: "authentication_challenge", category: "Code Snippet", command: ""},
+  { id: 7, name: "authentication_factor", category: "Code Snippet", command: ""},
+  { id: 8, name: "audit_logs/exports/:id", category: "Code Snippet", command: "GET"},
+  { id: 9, name: "audit_logs/exports/", category: "Code Snippet", command: "POST"},
+  { id: 10, name: "audit_logs_export", category: "Code Snippet", command: ""},
 
   //* - USER MANAGEMENT - *//
 
-  { id: 10, name: "Example apps", category: "User Management"},
-  { id: 11, name: "Magic Auth", category: "User Management"},
-  { id: 12, name: "Users and Organizations", category: "User Management"},
-  { id: 13, name: "AuthKit", category: "User Management"},
-  { id: 14, name: "Multi-Factor Authentication", category: "User Management"},
-  { id: 15, name: "Organization Authentication Policies", category: "User Management"},
-  { id: 16, name: "Configuring a domain", category: "User Management", subCat1: "Email Domains"},
-  { id: 17, name: "Migrate from another service", category: "User Management", subCat1: "Migrating to User Management"},
+  { id: 11, name: "Example apps", category: "User Management"},
+  { id: 12, name: "Magic Auth", category: "User Management"},
+  { id: 13, name: "Users and Organizations", category: "User Management"},
+  { id: 14, name: "AuthKit", category: "User Management"},
+  { id: 15, name: "Multi-Factor Authentication", category: "User Management"},
+  { id: 16, name: "Organization Authentication Policies", category: "User Management"},
+  { id: 17, name: "Configuring a domain", category: "User Management", subCat1: "Email Domains"},
+  { id: 18, name: "Migrate from another service", category: "User Management", subCat1: "Migrating to User Management"},
 
   //* - MIGRATIONS - *//
 
-  { id: 18, name: "Migrate from the standalone SSO API", category: "Migrations"},
-  { id: 19, name: "Migrate from Auth0", category: "Migrations"},
-  { id: 20, name: "Migrate from AWS Cognito", category: "Migrations"},
-  { id: 21, name: "Migrate users from another service", category: "Migrations", subCat1: "Migrate to WorkOS"},
-  { id: 22, name: "Exporting Auth0 user data", category: "Migrations", subCat1: "Migrate from Auth0"},
+  { id: 19, name: "Migrate from the standalone SSO API", category: "Migrations"},
+  { id: 20, name: "Migrate from Auth0", category: "Migrations"},
+  { id: 21, name: "Migrate from AWS Cognito", category: "Migrations"},
+  { id: 22, name: "Migrate users from another service", category: "Migrations", subCat1: "Migrate to WorkOS"},
+  { id: 23, name: "Exporting Auth0 user data", category: "Migrations", subCat1: "Migrate from Auth0"},
 
   //* - SINGLE SIGN-ON - *//
 
-  { id: 23, name: "Example apps", category: "Single Sign-On"},
-  { id: 24, name: "Choose your integration approach", category: "Single Sign-On", subCat1: "Single Sign-On"},
+  { id: 24, name: "Example apps", category: "Single Sign-On"},
+  { id: 25, name: "Choose your integration approach", category: "Single Sign-On", subCat1: "Single Sign-On"},
 
 
     // Add more options as needed
