d8861ff68f14b44dcfab981bb1153fbe7fe322d0
Norman Buruchara <normanburuchara90@gmail.com>
2024-06-12 16:45:55 +0300

    [CONTENT] Update: adding search terms for docs (User Management)
    Completed adding content in "Branding" subsection of User Management section in docs

--- src/home/desktop/d_Home_Pages/d_Documentation_Demo/User_Mgmt_Docs/User_Management_Search_Terms.js
+++ src/home/desktop/d_Home_Pages/d_Documentation_Demo/User_Mgmt_Docs/User_Management_Search_Terms.js
@@ -16,7 +16,7 @@ const results = [
 
     {id: "999925", name: "Email Domains", category: "User Management", subCat1: "Email Domains",  page: "Email Domains", lastCat: "top"},
 
-    //! - BRANDING - !//
+        //! - BRANDING - !//
 
     {id: "999908", name: "Branding", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "top"},
 
@@ -102,18 +102,6 @@ const results = [
     //? - - - - - - - - - - - - ?//
     //? - - - - P TAGS - - - - ?//
     //? - - - - - - - - - - - - ?//
-
-        //! - BRANDING - !//
-
-    {id: "999900", name: "Customize AuthKit to fit natively with your app’s unique design.", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "top"},
-    {id: "999989", name: "You can customize the look and feel of AuthKit via the Branding section of the WorkOS Dashboard", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
-    {id: "999988", name: "The brand editor allows you to:", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
-    {id: "999987", name: "Upload logos and favicons", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
-    {id: "999986", name: "Set brand colors for buttons, links, and backgrounds", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
-    {id: "999985", name: "Manage visual properties such as corner radius and dark mode appearance", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
-    {id: "999984", name: "The AuthKit preview will update in real-time as you make changes and accurately reflect the available authentication methods, giving you a clear picture of the authentication experience with AuthKit.", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
-
-    {id: "999983", name: "AuthKit supports both light and dark mode; each brand configuration option is split across both so that they can be configured independently. You can enforce a specific appearance, or allow the user's OS system settings to determine which to use.", category: "User Management", subCat1: "Branding", subCat2: "Appearance",  page: "Branding", lastCat: "Appearance"},
 
         //! - QUICK START - !//
 
@@ -207,7 +195,34 @@ const results = [
     {id: "999910", name: "It can take some time for DNS changes to take effect. If the initial verification attempt is not successful, WorkOS will continue trying to verify your domain for 72 hours.", category: "User Management", subCat1: "Email Domains", subCat2: "Create CNAME records",  page: "Email Domains", lastCat: "img8"},
     {id: "999909", name: "Once your domain is successfully verified, authentication emails and Admin Portal invites will be sent from no-reply@your-domain.com. It’s important to keep the CNAME records in place to ensure that WorkOS can deliver mail on your behalf.", category: "User Management", subCat1: "Email Domains", subCat2: "Create CNAME records",  page: "Email Domains", lastCat: "img8"},
 
+        //! - BRANDING - !//
 
+    {id: "999900", name: "Customize AuthKit to fit natively with your app’s unique design.", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "top"},
+    {id: "999989", name: "You can customize the look and feel of AuthKit via the Branding section of the WorkOS Dashboard", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+    {id: "999988", name: "The brand editor allows you to:", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+    {id: "999987", name: "Upload logos and favicons", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+    {id: "999986", name: "Set brand colors for buttons, links, and backgrounds", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+    {id: "999985", name: "Manage visual properties such as corner radius and dark mode appearance", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+    {id: "999984", name: "The AuthKit preview will update in real-time as you make changes and accurately reflect the available authentication methods, giving you a clear picture of the authentication experience with AuthKit.", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+
+    {id: "999983", name: "AuthKit supports both light and dark mode; each brand configuration option is split across both so that they can be configured independently. You can enforce a specific appearance, or allow the user's OS system settings to determine which to use.", category: "User Management", subCat1: "Branding", subCat2: "Appearance",  page: "Branding", lastCat: "Appearance"},
+    {id: "999982", name: "The corner radius applied to UI elements can also be configured; a lower value will result in a more formal aesthetic while a higher value has a more rounded, playful feel.", category: "User Management", subCat1: "Branding", subCat2: "Appearance",  page: "Branding", lastCat: "Appearance"},
+
+    {id: "999981", name: "You can upload custom brand assets to display in AuthKit, transactional emails, and the Admin Portal", category: "User Management", subCat1: "Branding", subCat2: "Assets",  page: "Branding", lastCat: "Assets"},
+
+    {id: "999980", name: "There are three types of uploadable assets:", category: "User Management", subCat1: "Branding", subCat2: "Assets",  page: "Branding", lastCat: "Assets"},
+    {id: "999979", name: "Logo: Your full size brand logo, styles vary but this would typically include the wordmark.", category: "User Management", subCat1: "Branding", subCat2: "Assets",  page: "Branding", lastCat: "Assets"},
+    {id: "999978", name: "Logo icon: A smaller, iconized version of the logo, this is often simply the logomark.", category: "User Management", subCat1: "Branding", subCat2: "Assets",  page: "Branding", lastCat: "Assets"},
+    {id: "999977", name: "Favicon: The favicon is displayed in the browser tab alongside the address bar.", category: "User Management", subCat1: "Branding", subCat2: "Assets",  page: "Branding", lastCat: "Assets"},
+
+    {id: "999976", name: "Either the logo or the logo icon can be displayed in AuthKit. To select which to use, click the logo in the AuthKit preview after uploading both assets.", category: "User Management", subCat1: "Branding", subCat2: "Assets", subCat3: "Logo style",  page: "Branding", lastCat: "Logo style"},
+
+    {id: "999975", name: "You can control four colors across light and dark mode:", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+    {id: "999974", name: "Page background color", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+    {id: "999973", name: "Button background colors", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+    {id: "999972", name: "Button text color", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+    {id: "999971", name: "Link color", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+    {id: "999970", name: "Other colors used in the UI, like the focus outline, hover styles, or borders, are created automatically based on the four colors you provide, ensuring a consistent look and feel.", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
 
 ]
 
---
+++ src/home/desktop/d_Home_Pages/d_Documentation_Demo/User_Mgmt_Docs/User_Mgmt_Terms/Branding.js
@@ -0,0 +1,65 @@
+const results = [
+
+    //? - - - - - - - - - - - - - ?//
+    //? - - H1 (TOP) HEADERS - - ?//
+    //? - - - - - - - - - - - - - ?//
+
+       //! - BRANDING - !//
+
+    {id: "999908", name: "Branding", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "top"},
+
+    //? - - - - - - - - - - - - - - - ?//
+    //? - - H1 (IN-TEXT) HEADERS - - ?//
+    //? - - - - - - - - - - - - - - - ?//
+
+    {id: "999907", name: "Introduction", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "Introduction"},
+    {id: "999906", name: "Appearance", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "Appearance"},
+    {id: "999905", name: "Assets", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "Assets"},
+    {id: "999904", name: "Color", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "Color"},
+    {id: "999903", name: "Copy", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "Copy"},
+
+
+    //? - - - - - - - - - - - - - - ?//
+    //? - - - - H3 HEADERS - - - - ?//
+    //? - - - - - - - - - - - - - - ?//
+
+        //! - BRANDING - !//
+
+    {id: "999902", name: "Logo style", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "Logo style"},
+
+    //? - - - - - - - - - - - - ?//
+    //? - - - - P TAGS - - - - ?//
+    //? - - - - - - - - - - - - ?//
+
+        //! - BRANDING - !//
+
+    {id: "999900", name: "Customize AuthKit to fit natively with your app’s unique design.", category: "User Management", subCat1: "Branding",  page: "Branding", lastCat: "top"},
+    {id: "999989", name: "You can customize the look and feel of AuthKit via the Branding section of the WorkOS Dashboard", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+    {id: "999988", name: "The brand editor allows you to:", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+    {id: "999987", name: "Upload logos and favicons", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+    {id: "999986", name: "Set brand colors for buttons, links, and backgrounds", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+    {id: "999985", name: "Manage visual properties such as corner radius and dark mode appearance", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+    {id: "999984", name: "The AuthKit preview will update in real-time as you make changes and accurately reflect the available authentication methods, giving you a clear picture of the authentication experience with AuthKit.", category: "User Management", subCat1: "Branding", subCat2: "Introduction",  page: "Branding", lastCat: "Introduction"},
+
+    {id: "999983", name: "AuthKit supports both light and dark mode; each brand configuration option is split across both so that they can be configured independently. You can enforce a specific appearance, or allow the user's OS system settings to determine which to use.", category: "User Management", subCat1: "Branding", subCat2: "Appearance",  page: "Branding", lastCat: "Appearance"},
+    {id: "999982", name: "The corner radius applied to UI elements can also be configured; a lower value will result in a more formal aesthetic while a higher value has a more rounded, playful feel.", category: "User Management", subCat1: "Branding", subCat2: "Appearance",  page: "Branding", lastCat: "Appearance"},
+
+    {id: "999981", name: "You can upload custom brand assets to display in AuthKit, transactional emails, and the Admin Portal", category: "User Management", subCat1: "Branding", subCat2: "Assets",  page: "Branding", lastCat: "Assets"},
+
+    {id: "999980", name: "There are three types of uploadable assets:", category: "User Management", subCat1: "Branding", subCat2: "Assets",  page: "Branding", lastCat: "Assets"},
+    {id: "999979", name: "Logo: Your full size brand logo, styles vary but this would typically include the wordmark.", category: "User Management", subCat1: "Branding", subCat2: "Assets",  page: "Branding", lastCat: "Assets"},
+    {id: "999978", name: "Logo icon: A smaller, iconized version of the logo, this is often simply the logomark.", category: "User Management", subCat1: "Branding", subCat2: "Assets",  page: "Branding", lastCat: "Assets"},
+    {id: "999977", name: "Favicon: The favicon is displayed in the browser tab alongside the address bar.", category: "User Management", subCat1: "Branding", subCat2: "Assets",  page: "Branding", lastCat: "Assets"},
+
+    {id: "999976", name: "Either the logo or the logo icon can be displayed in AuthKit. To select which to use, click the logo in the AuthKit preview after uploading both assets.", category: "User Management", subCat1: "Branding", subCat2: "Assets", subCat3: "Logo style",  page: "Branding", lastCat: "Logo style"},
+
+    {id: "999975", name: "You can control four colors across light and dark mode:", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+    {id: "999974", name: "Page background color", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+    {id: "999973", name: "Button background colors", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+    {id: "999972", name: "Button text color", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+    {id: "999971", name: "Link color", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+    {id: "999970", name: "Other colors used in the UI, like the focus outline, hover styles, or borders, are created automatically based on the four colors you provide, ensuring a consistent look and feel.", category: "User Management", subCat1: "Branding", subCat2: "Color",  page: "Branding", lastCat: "Color"},
+
+];
+
+export default results;
