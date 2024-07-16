33d7451ca0efa0cfc79c0eb62aee0a49030ad23e
Norman Buruchara <normanburuchara90@gmail.com>
2024-07-16 14:07:23 +0300

    [WRAPPING UP] : overall project

--- src/home/desktop/d_Home_Pages/d_Organizations/d_Organizations.js
+++ src/home/desktop/d_Home_Pages/d_Organizations/d_Organizations.js
@@ -3,6 +3,31 @@ import styled from 'styled-components'
 
 const Styles = styled.div `
 
+    // - - - - - - - PARENT CONTAINER - - - - - - - //
+
+    // - - PAGE HEADER - - //
+
+.pageHeader {
+    text-align: left;
+    margin-left: 5%;
+    // margin-top: 7.25%;
+}
+
+.pageHeader h1 {
+    margin-top: 1%;
+    font-family: poppins;
+}   
+
+.pageHeader h3 {
+    margin-top: 1%;
+    font-family: poppins;
+}  
+
+.pageHeader p {
+    margin-top: 1%;
+    font-family: poppins;
+}
+
 `
 
 export default class DesktopOrganizations extends Component {
@@ -16,8 +41,12 @@ export default class DesktopOrganizations extends Component {
     render () {
         return (
             <Styles>
-                <div>
-                    <h1>Organizations page</h1>
+                <div className='parent_container'>
+                    <div className='pageHeader'>
+                        <h1 className=''>Organizations</h1>
+                        <h3>[Empty page]</h3>
+                        <p>Click "Docs" to see the quick access documentation demo.</p>
+                    </div>
                 </div>
             </Styles>
         )
